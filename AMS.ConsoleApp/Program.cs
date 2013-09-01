8a3afae2723762d9541abff068ddee1a4cbefed2
SirwanAfifi <sir1afifi@gmail.com>
2013-09-01 23:14:31 +0430

    Database Initializer

---
+++ AMS.ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using AMS.Data;
+
+namespace AMS.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataContext context = new DataContext();
+            context.Database.Initialize(true);
+        }
+    }
+}
