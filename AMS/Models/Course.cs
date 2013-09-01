d04a20ee59054fcfdcf03a598698f59ff9ad2740
Abhishek Luv <abhishek.luv@outlook.com>
2013-09-01 08:16:58 +0530

    Course is not a person
    Course is not a person. So i have created a new Id property for Course.cs

--- AMS/Models/Course.cs
+++ AMS/Models/Course.cs
@@ -6,8 +6,10 @@ using System.Web;
 
 namespace AMS.Models
 {
-    public class Course : Person
+    public class Course
     {
+        [Required]
+        public int Id { get; set;}
         public string CourseName { get; set; }
 
         public ICollection<Student> Students { get; set; }
